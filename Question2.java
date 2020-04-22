1c8d7a3ed18242f76fbd7eabd73583927c296ebd
Joel Yeo <44794609+joelchunsheng@users.noreply.github.com>
2020-04-22 09:25:24 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -4,13 +4,13 @@ public class Question2
 {
   public static void main(String[] args)
   {
-    /**
-     * Prompts user for height and weight, calculate and print out the height and weight of the user
-     * 
-     * Hint: in.nextDouble() to read double
-     */
-     
     Scanner in = new Scanner(System.in);
+    System.out.print("Please enter height (cm): ");
     double height = in.nextDouble();
+
+    System.out.print("Please enter weight (kg): ");
+    double weight = in.nextDouble();
+
+    System.out.format("Height : %.2f cm\nWeight : %.2f kg", height, weight);
   }
 }
