2d00b0b83596dbe9c80ce5002878a2e85dac8551
Joel Yeo <44794609+joelchunsheng@users.noreply.github.com>
2020-04-22 13:33:15 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -4,29 +4,47 @@ public class Question5
 {
   public static void main(String[] args)
   {
-    /**
-     * Prompt the user for number. This input indicates the number of integers the user will be entering next. 
-     * Print out the mode (highest occurrence) from the set of integers. 
-     *    e.g.
-     *     > 5
-     *     > 2
-     *     > 4
-     *     > 1
-     *     > 3
-     *     > 4
-     *     4
-     * 
-     *    e.g.
-     *     > 4
-     *     > 2
-     *     > 2
-     *     > 3
-     *     > 3
-     *     2
-     * Hint: Use a loop to get input. Use another 2 loops to find the mode
-     */
-     
+    ArrayList<Integer> integerList = new ArrayList<>();
+
     Scanner in = new Scanner(System.in);
+
+    System.out.print("Number of integers : ");
+    int numberInteger = in.nextInt();
+
+    for (int q = 0; q < numberInteger; q++){
+      System.out.print("Enter integer : ");
+      int number = in.nextInt();
+      integerList.add(number);
+    }
+    
+    // Reverse list
+    Collections.reverse(integerList);
+
+    int n = integerList.size();
+    
+    int maxCount = 1; 
+    int currentCount = 1; 
+    int mode = integerList.get(0);
+
+    for (int i = 1; i < n; i++) {
+      //if values match, increase currentCount
+      if (integerList.get(i) == integerList.get(i-1)) 
+        currentCount++; 
+      //if values don't match, save and move on
+      else{ 
+        maxCount = currentCount; 
+        mode = integerList.get(i-1); 
+        currentCount = 1; 
+        } 
+      } 
+
+    // checks for last value 
+    if (currentCount >= maxCount){ 
+      maxCount = currentCount; 
+      mode = integerList.get(n-1); 
+    } 
+
+    System.out.print("Mode: " + mode);
     
   }
 }
